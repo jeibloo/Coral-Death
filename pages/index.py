32901a83b8012dc06e11c7db7be3664b16ab5a44
Jason <jasonnova@protonmail.com>
2019-08-26 12:25:53 -0700

    3D map added

--- pages/index.py
+++ pages/index.py
@@ -10,17 +10,17 @@ from app import app
 """
 https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
 
-Layout in Bootstrap is controlled using the grid system. The Bootstrap grid has 
+Layout in Bootstrap is controlled using the grid system. The Bootstrap grid has
 twelve columns.
 
-There are three main layout components in dash-bootstrap-components: Container, 
+There are three main layout components in dash-bootstrap-components: Container,
 Row, and Col.
 
 The layout of your app should be built as a series of rows of columns.
 
-We set md=4 indicating that on a 'medium' sized or larger screen each column 
-should take up a third of the width. Since we don't specify behaviour on 
-smaller size screens Bootstrap will allow the rows to wrap so as not to squash 
+We set md=4 indicating that on a 'medium' sized or larger screen each column
+should take up a third of the width. Since we don't specify behaviour on
+smaller size screens Bootstrap will allow the rows to wrap so as not to squash
 the content.
 """
 
@@ -28,7 +28,7 @@ column1 = dbc.Col(
     [
         dcc.Markdown(
             """
-        
+
             ## Value Proposition
 
             THIS APP SHALLE BENEFIT U. Don't emphasize the underlying technology.
@@ -45,8 +45,9 @@ column1 = dbc.Col(
 )
 
 gapminder = px.data.gapminder()
-fig = px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp", size="pop", color="continent",
-           hover_name="country", log_x=True, size_max=60)
+fig = px.line_geo(gapminder.query("year==2007"),
+                  locations="iso_alpha", color="continent",
+                  projection="orthographic")
 
 column2 = dbc.Col(
     [
