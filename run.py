1a4520211415ea94d73564473c46ecdf0453db30
Jason <jasonnova@protonmail.com>
2019-09-22 10:58:12 -0700

    Added logo

--- run.py
+++ run.py
@@ -47,6 +47,7 @@ footer = dbc.Container(
                     html.A(html.I(className='fab fa-github-square mr-3'), href='https://github.com/jeibloo/coral-death'),
                     html.A(html.I(className='fab fa-linkedin mr-3'), href='https://www.linkedin.com/in/jason-nova/'),
                     html.A(html.I(className='fab fa-twitter-square mr-3'), href='https://twitter.com/jsn404'),
+                    html.Img(className='logo',src=app.get_asset_url('logo.png')),
                 ],
                 className='lead',
             )
